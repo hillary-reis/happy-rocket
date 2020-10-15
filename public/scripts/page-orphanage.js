ed409353a75d4979f20b2d74c9d0ddc22a78dfbe
Hillary Reis <hillary.reis2002@gmail.com>
2020-10-15 20:22:38 -0300

    segundo dia

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,45 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false
+}
+
+const map = L.map('mapid', options).setView([-27.2198863,-49.6478308], 15);
+
+L.tileLayer ('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+const icon = L.icon ({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+});
+
+L.marker ([-27.2198863,-49.6478308], { icon }).addTo(map);
+
+
+/*=== image galley ===*/
+
+function selectImage (event) {
+  const button = event.currentTarget;
+
+  // remover todas as classes active
+  const buttons = document.querySelectorAll ('.images button');
+  buttons.forEach(removeActiveClass);
+
+  function removeActiveClass (button) {
+    button.classList.remove ('active');
+  };
+
+  // selecionar a imagem clicada
+  const image = button.children[0];
+  const imageContainer = document.querySelector ('.orphanage-details > img');
+
+  // atualizar o container de image
+  imageContainer.src = image.src;
+
+  // adicionar a classe active para este botão
+  button.classList.add ('active');
+};
